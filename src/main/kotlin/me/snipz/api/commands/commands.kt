314b850a5553a778bf486c4112ed3cad6bae0684
devoirr <devoirr@users.noreply.github.com>
2025-06-12 14:59:56 +0300

    Added commands utility.

---
+++ src/main/kotlin/me/snipz/api/commands/commands.kt
@@ -0,0 +1,95 @@
+package me.snipz.api.commands
+
+import org.bukkit.command.CommandSender
+import org.bukkit.command.defaults.BukkitCommand
+import org.bukkit.entity.Player
+
+typealias PlayerExecutor = (Player, List<String>) -> Unit
+typealias GenericExecutor = (CommandSender, List<String>) -> Unit
+typealias TabCompleteProvider = (CommandSender, List<String>) -> List<String>
+
+fun command(name: String, init: CommandBuilder.() -> Unit): BukkitCommand {
+    val builder = CommandBuilder(name)
+    builder.init()
+
+    return builder.build()
+}
+
+class CommandBuilder(
+    val name: String,
+    var permission: String = "",
+    val aliases: MutableList<String> = mutableListOf(),
+    private var playerExecutor: PlayerExecutor? = null,
+    private var genericExecutor: GenericExecutor? = null,
+    private var tabCompleter: TabCompleteProvider? = null,
+    private val subcommands: MutableMap<String, CommandBuilder> = mutableMapOf()
+) {
+    fun playerExecutor(block: PlayerExecutor) {
+        playerExecutor = block
+    }
+
+    fun executor(block: GenericExecutor) {
+        genericExecutor = block
+    }
+
+    fun tabCompleter(block: TabCompleteProvider) {
+        tabCompleter = block
+    }
+
+    fun subcommand(name: String, block: CommandBuilder.() -> Unit) {
+        val sub = CommandBuilder(name)
+        sub.apply(block)
+        subcommands[name] = sub
+    }
+
+    fun build(): BukkitCommand {
+        val cmd = object : BukkitCommand(name) {
+            override fun execute(sender: CommandSender, label: String, args: Array<out String>): Boolean {
+                if (args.isNotEmpty() && subcommands.containsKey(args[0])) {
+                    val sub = subcommands[args[0]]!!
+                    sub.execute(sender, args.drop(1))
+                    return true
+                }
+                execute(sender, args.toList())
+                return true
+            }
+
+            override fun tabComplete(sender: CommandSender, alias: String, args: Array<out String>): List<String> {
+                if (args.isNotEmpty() && subcommands.containsKey(args[0])) {
+                    val sub = subcommands[args[0]]!!
+                    return sub.tabComplete(sender, alias, args.drop(1))
+                }
+                return tabCompleter?.invoke(sender, args.toList()) ?: emptyList()
+            }
+        }
+
+        cmd.permission = permission
+        cmd.aliases = aliases
+
+        return cmd
+    }
+
+    private fun execute(sender: CommandSender, args: List<String>) {
+        when {
+            sender is Player && playerExecutor != null -> {
+                playerExecutor?.invoke(sender, args)
+            }
+
+            genericExecutor != null -> {
+                genericExecutor?.invoke(sender, args)
+            }
+
+            else -> {
+                sender.sendMessage("Эта команда доступна только игрокам.")
+            }
+        }
+    }
+
+    private fun tabComplete(sender: CommandSender, alias: String, args: List<String>): List<String> {
+        if (args.isNotEmpty() && subcommands.containsKey(args[0])) {
+            val sub = subcommands[args[0]]!!
+            return sub.tabComplete(sender, alias, args.drop(1))
+        }
+        return tabCompleter?.invoke(sender, args) ?: emptyList()
+    }
+}
